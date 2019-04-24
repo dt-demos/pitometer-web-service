ae149f31e4f33e900b0023a36b9d0f4c2e7e0510
rob jahn <robjahn@gmail.com>
2019-04-24 10:08:54 -0400

    add health url

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // routes =======================================================================
+var router = express.Router();
 app.use('/api', require('./routes/routes').router);
+//app.use('/api', router);
+app.use("/health", router);
+
+router.get('/', function (req, res, next) {
+  res.json({status: 'UP'});
+});
 
 // listen (start app with node server.js) 
 app.listen(process.env.PORT || 8080, () => {
